6e9b3a3ef0810b740ded44bf4743e3ddaadb411d
Travon <zhouyanbai@qq.com>
2021-12-04 12:41:58 +0800

    add_plan

---
+++ RelaSim.mm
@@ -0,0 +1,260 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="RelaSim" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1638589117051">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="事实" POSITION="right" ID="ID_1927691732" CREATED="1638589121714" MODIFIED="1638589834498">
+<node TEXT="光速不变" ID="ID_873966385" CREATED="1638589760395" MODIFIED="1638589832316"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      我们在能找到的各种参考系中观测到的光速始终不变，值被定义为299792458m/s
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="相对性原则" ID="ID_43898361" CREATED="1638589761249" MODIFIED="1638590157341"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      观察者具有相等性，物理定律在不同参考系下的表现相同。即，不能通过观察区分不同的参考系
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="等效性原则" ID="ID_678420342" CREATED="1638589761404" MODIFIED="1638590800920"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      对非惯性参考系与引力场的处理
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="模拟实现" POSITION="left" ID="ID_1485735725" CREATED="1638590852429" MODIFIED="1638590862902">
+<node TEXT="参考系" ID="ID_1806395582" CREATED="1638590877603" MODIFIED="1638591277595">
+<node TEXT="可以容纳运动" ID="ID_159251434" CREATED="1638591279401" MODIFIED="1638591348461"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      需要一个容器存储运动实体
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="" ID="ID_1136364431" CREATED="1638591965740" MODIFIED="1638591965741">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="时间" ID="ID_643797580" CREATED="1638591876796" MODIFIED="1638592030387"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      由该参考系维护的时间
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="空间" ID="ID_661649043" CREATED="1638591888563" MODIFIED="1638592033222"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      由该参考系维护的空间
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="" ID="ID_684552551" CREATED="1638591965738" MODIFIED="1638591965740">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="所有的参考系时空原点应相等，或指定其他转换关系" ID="ID_1508143909" CREATED="1638591965741" MODIFIED="1638592017198"/>
+</node>
+<node TEXT="运动实体" ID="ID_111122759" CREATED="1638591371020" MODIFIED="1638591468960"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      需包含足够的要素以完整描述一个运动，由这类要素可以得知运动实体在该参考系下任意时刻T的空间坐标
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="ID" ID="ID_325969887" CREATED="1638591824984" MODIFIED="1638591855843"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      由参考系与自身ID组合
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="T0空间坐标" ID="ID_1014504791" CREATED="1638591499551" MODIFIED="1638591511064"/>
+<node TEXT="T0速度" ID="ID_1029330456" CREATED="1638591513696" MODIFIED="1638591530162"/>
+<node TEXT="加速度函数F(t)" ID="ID_1312502236" CREATED="1638591531144" MODIFIED="1638591572918"/>
+</node>
+</node>
+<node TEXT="可以观察其他参考系中的运动" ID="ID_1451016858" CREATED="1638591284315" MODIFIED="1638591364590"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      需要定义对其他参考系中运动实体的变换
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="光速" ID="ID_1219113058" CREATED="1638590883596" MODIFIED="1638590885578">
+<node TEXT="给定常量" ID="ID_1224148044" CREATED="1638591053679" MODIFIED="1638592142968"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      用以计算变换
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="运动" ID="ID_1079225650" CREATED="1638590891700" MODIFIED="1638590895019">
+<node TEXT="依附于某一参考系，不能独立存在" ID="ID_1878744480" CREATED="1638591077663" MODIFIED="1638591160339"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      任选一参考系来依附，可叫做base或origin
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="在不同参考系表现可以不同" ID="ID_1944676062" CREATED="1638591102600" MODIFIED="1638591224655"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      不同参考系观察运动时，等于对运动施加一个变换，这个变换由两参考系的相对运动决定
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
